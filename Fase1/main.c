7edfd91ab2a0baaf832c9fd095475ef58aff7664
lupaz <eslupaz@gmail.com>
2016-08-02 23:43:10 -0600

    segundo commit

--- Fase1/main.c
+++ Fase1/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct{
+    int numeroToken;
+    struct tokens *siguiente;
+}tokens;
+
+
+
 int main()
 {
     printf("Hola este es el primer Commit!\n");
+
+
+    printf("antes:\n\n");
+
+    tokens *tok1= malloc(sizeof(tokens));
+    tok1->numeroToken=1;
+
+    printf(" el token es %i \n",tok1->numeroToken);
+    printf("llego");
     return 0;
 }
