2b5e850632bfc3c9c21012e7d609b62f77f83b80
lupaz <eslupaz@gmail.com>
2016-08-08 00:05:07 -0600

    nuevo commit

--- Fase1/main.c
+++ Fase1/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define VAL 80
 
@@ -14,20 +16,36 @@ typedef struct{
     char palabra[10];
 }reservadas;
 
+typedef struct rutas{
+    char nombre[25];
+    struct rutas *siguiente;
+}rutas;
+
+
+
 //=======================================================================================Variables globales===========================================================================================
 reservadas palabrasRes[17];
 tokens *cabezaT;
 tokens *colaT;
+rutas *cabezaR;
+rutas *colaR;
 char linea[VAL]; //sera la encargada de contener toda la linea leida para luego analizarla por partes
 tokens * tokActual;
 tokens * preanalis;
 int tokenActual=0;
 int preanalisis=0;
-//====== variables de parametros =======
+//=============== variables de parametros =================
 int size=0;
 char path[50];
 char name[50];
-char unidad='\0';
+char unidad[2];
+int error=0;
+//================variables banderas ======================
+int Bsize=0;
+int Bname=0;
+int Buni=0;
+int Bpath=0;
+int tamano=0;
 
 //===================================================================================Declaracion de Metodos===========================================================================================
 void anlisisLexico( char *linea );
@@ -50,6 +68,24 @@ int segundaLinea(char linea[]);
 
 int esReservada(char lexema[]);
 
+void QuitarComillas();
+
+void crearCarpeta(char ruta[]);
+
+void agregarRuta(char ruta[]);
+
+void splitPath(char cadena[]);
+
+void imprimirRutas();
+
+//=========== metodos sintacticos =============
+void avanzarToken();
+void origen();
+void COM1();
+void PAR();
+void PAR0();
+void PAR1();
+void realizarAcciones();
 //===========================================================================================Metodo Principal===========================================================================================
 int main(){
     cargarPalabrasReservadas();
@@ -69,16 +105,13 @@ int main(){
             tokenActual=cabezaT->token;
             tokActual=cabezaT;
         }
-        if(cabezaT->siguiente!=NULL){
-            preanalisis=cabezaT->siguiente->token;
-            preanalis=cabezaT->siguiente;
-        }
         imprimirTokens();
+        realizarAcciones();
         if(strcmp(linea,"exit")==0){
             salida=1;
         }
 
-    }while(salida!=1);
+    }while(salida!=1);//*/
 
     return 0;
 
@@ -221,12 +254,12 @@ void analisisLexico(char linea[]){
                     estado=0;
                 }else if(caracter=='\0'){
                     lexema[aux]='\0'; //cerramos la cadena
-                    addToken(2,lexema); //guardamos el token y su lexema para el analisis sintactico
+                    addToken(1,lexema); //guardamos el token y su lexema para el analisis sintactico
                     aux=0;
                     estado=0;
                 }else{
                     lexema[aux]='\0'; //cerramos la cadena
-                    addToken(2,lexema); //guardamos el token y su lexema para el analisis sintactico
+                    addToken(1,lexema); //guardamos el token y su lexema para el analisis sintactico
                     aux=0;
                     estado=0;
                     puntero-=1;
@@ -264,10 +297,16 @@ void analisisLexico(char linea[]){
                     }
                 }else{
                     lexema[aux]='\0'; //cerramos la cadena
-                    addToken(2,lexema); //guardamos el token y su lexema para el analisis sintactico
-                    aux=0;
-                    estado=0;
-                    puntero-=1;
+                    if(esReservada(lexema)==1){
+                        aux=0;
+                        estado=0;
+                        puntero-=1;
+                    }else{
+                        addToken(2,lexema);
+                        aux=0;
+                        estado=0;
+                        puntero-=1;
+                    }
                 }
                 break;
             }
@@ -525,22 +564,173 @@ int segundaLinea(char linea[]){
     }
     return 0;
 }
+
+//=========================carpetas===========================
+
+void QuitarComillas(char cad[]){
+    int i;
+    int j=0;
+    if(cad[0]=='"' && cad[strlen(cad)-1]=='"'){
+        for(i=0;i<strlen(cad);i++){
+            if(cad[i]!='"'){
+                cad[j]=cad[i];
+                j++;
+            }
+            if(i==(strlen(cad)-2)){
+                cad[i]='\0';
+            }
+        }
+    }
+}
+
+void crearCarpeta(char ruta[]){ //recibe la rutas ya sin comillas
+    char comando[50]="sudo mkdir ";
+    strcat(comando,ruta);
+    system(comando);
+}
+
+void agregarRuta(char ruta[]){
+    if(cabezaR==NULL){
+        cabezaR = malloc(sizeof(rutas));
+        strcpy(cabezaR->nombre,ruta);
+        cabezaR->siguiente=NULL;
+        colaR=cabezaR;
+    }else{
+        colaR->siguiente=malloc(sizeof(rutas));
+        strcpy(colaR->siguiente->nombre,ruta);
+        colaR=colaR->siguiente;
+    }
+}
+
+void splitPath(char cadena[]){
+    char lexema[25];
+    lexema[0]='\0';
+    char caracter;
+    int aux=0;
+    int i=0;
+    for(;i<strlen(cadena);i++){
+        caracter=cadena[i];
+        if(caracter=='/'){
+            if(lexema[0]!='\0'){
+                lexema[aux]='\0';
+                aux=0;
+                agregarRuta(lexema);
+            }
+        }else{
+            if(caracter==' '){
+                caracter='_';
+            }
+            lexema[aux]=caracter;
+            aux++;
+            if(i==(strlen(cadena)-1)){
+                lexema[aux]='\0';
+                aux=0;
+                agregarRuta(lexema);
+            }
+        }
+    }
+}
+
+void imprimirRutas(){
+    if(cabezaR==NULL){
+        printf("Tabla de rutas vacia.\n");
+    }else{
+        rutas *tmp=cabezaR;
+        printf("%8s","Carpetas\n");
+        while(tmp!=NULL){
+        printf("%7s\n",tmp->nombre);
+        tmp=tmp->siguiente;
+        }
+    }
+}
+
+void crearDirectorios(char path[]){
+    QuitarComillas(path);
+    splitPath(path);
+    char general[50]="/";
+
+    if(cabezaR!=NULL){
+        rutas *tmp;
+        tmp=cabezaR;
+        while(tmp!=NULL){
+            strcat(general,tmp->nombre);
+            if(existeCarpeta(general)==2){
+                crearCarpeta(general);
+            }
+            strcat(general,"/");
+        tmp=tmp->siguiente;
+        }
+    }
+}
+
+int existeCarpeta(char ruta[]){
+    struct stat estado;
+    if(stat(ruta,&estado)==-1){
+        return 2;
+    }else{
+        return 1;
+    }
+}
+
+//========================Archivos sdk==========================
+
+
+
 //=============================================================================Metodos de analisis sintactico===========================================================================================
+
 void avanzarToken(){
-    tokActual=tokActual->siguiente;
-    tokenActual=tokActual->token;
-    preanalis=tokActual->siguiente;
-    preanalisis=preanalis->token;
+    if(tokActual!=NULL){
+        if(tokActual->siguiente!=NULL){
+            tokActual=tokActual->siguiente;
+            tokenActual=tokActual->token;
+        }else{
+            tokActual=NULL;
+            tokenActual=0;
+        }
+    }
+    //preanalis=tokActual->siguiente;
+    //preanalisis=preanalis->token;
 }
 
 
 void origen(){ //inicio del analisis sintactico y validaciones
     if(tokActual!=NULL){
          switch (tokenActual) {
-
             case 9:{
                 avanzarToken();
                 COM1();
+                if(error==0){ // aca pienso hacer las validaciones y la creacion de los archs
+                    if(Buni=1){
+                            if(unidad=="m"|| unidad=="M" || unidad=="k"||unidad=="K"){
+                                if (unidad=="m"|| unidad=="M"){
+                                    tamano=1024*1024*size;
+                                }else{
+                                    tamano=1024*size;
+                                }
+                            }else{
+                                 tamano=1024*1024*size;
+                                 printf("ERROR: El parametro de unit no es valido.\n");
+                            }
+                        }else{
+                            tamano=1024*1024*size;
+                        }
+                    if(Bpath==0 || Bsize==0 || Bname==0){
+                        if(Bpath==0){
+                            printf("ERROR: Falta el parametro Path.\n");
+                        }else if( Bname==0){
+                            printf("ERROR: Falta el parametro Name.\n");
+                        }else{
+                            printf("ERROR: Falta el parametro Size.\n");
+                        }
+                        printf("AVISO: No se puede crear el disco.\n");
+                    }else{
+
+                        printf("Disco %s, de capacidad %i con unidades %s en la ruta %s.\n",name,size,unidad,path);
+
+                    }
+                }else{
+                   printf("AVISO: No se puede crear el disco.\n");
+                }
             break;
             }
             case 14:{
@@ -560,7 +750,7 @@ void origen(){ //inicio del analisis sintactico y validaciones
             break;
             }
             default:{
-                printf("No se reconocio ningun comnado inicial.\n");
+                printf("No se reconocio ningun comando inicial valido.\n");
             break;
             }
         }
@@ -578,16 +768,17 @@ void PAR(){
         switch (tokenActual) {
             case 6:{
                 avanzarToken();
-                PAR0();
+                PAR1();
             break;
             }
             case 7:{
                 avanzarToken();
-                PAR1();
+                PAR0();
             break;
             }
             default:{
-                printf("Los atributos deben iniciar con \"+\" o \"-\".\n");
+                printf("Los parametros deben iniciar con \"+\" o \"-\".\n");
+                error=1;
                 //podria hacer un ciclo que me llegue a un punto de recuperacion de errores pero por el momenot vamos a asumir que no se ejecutara nada si no esta sintacticamente bien;
             break;
             }
@@ -600,33 +791,95 @@ void PAR0(){
     if(tokActual!=NULL){
         switch (tokenActual) {
             case 10:{
-                avanzarToken();
-                if(tokenActual==5){
+                if(Bsize<1){
+                    Bsize=1;
                     avanzarToken();
+                    if(tokenActual==5){ // token = ::
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba :: despues de size.\n");
+                        break;
+                    }
+                    if(tokenActual==1){ //token = numero
+                        //si todo va bien aca guardaria el tamano size en una global
+                        size=atoi(tokActual->lexema); // global
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba un numero positivo despues de ::.\n");
+                        break;
+                    }
+                    PAR();
                 }else{
-                    avanzarToken();
-                    printf("se esperaba :: despues de size.\n");
-                    break;
+                   printf("El parametro size no se puede repetir.\n");
+                   error=1;
                 }
-                if(tokenActual==1){
-                    //si todo va bien aca guardaria el tamano size en una global
-                    avanzarToken();
-                }else{
-                    avanzarToken();
-                    printf("se esperaba un numero positivo despues de ::.\n");
-                    break;
-                }
-
             break;
             }
             case 12:{
-                avanzarToken();
-                //if()
-                PAR0();
+                if(Bpath<1){
+                    Bpath=1;
+                    avanzarToken();
+                    if(tokenActual==5){ // token = ::
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba :: despues de path.\n");
+                        break;
+                    }
+                    if(tokenActual==4){
+                        //si todo va bien aca guardaria el tamano size en una global
+                        strcpy(path,tokActual->lexema); // global
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba un numero positivo despues de \"::\".\n");
+                        break;
+                    }
+                    PAR();
+                }else{// aca notifico error cuando path viene mas de una vez
+                    printf("El parametro phat no se puede repetir.\n");
+                    error=1;
+                }
+            break;
+            }
+            case 13:{
+                if(Bname<1){
+                    Bname=1;
+                    avanzarToken();
+                    if(tokenActual==5){ // token = ::
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba :: despues de path.\n");
+                        break;
+                    }
+                    if(tokenActual==4){
+                        //si todo va bien aca guardaria el tamano size en una global
+                        strcpy(name,tokActual->lexema); // global
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("Se esperaba una cadena despues de \"::\".\n");
+                        break;
+                    }
+                    PAR();
+                }else{
+                    printf("El parametro name no se puede repetir.\n");
+                    error=1;
+                }
             break;
             }
             default:{
                 printf("El paramentro despues de \"-\" no es correcto.\n");
+                error=1;
             break;
             }
 
@@ -635,7 +888,51 @@ void PAR0(){
 }
 
 void PAR1(){
+    if(tokActual!=NULL){
+        switch (tokenActual) {
+            case 11:{
+                if(Buni<1){
+                    Buni=1;
+                    avanzarToken();
+                    if(tokenActual==5){ // token = ::
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba :: despues de unit.\n");
+                        break;
+                    }
+                    if(tokenActual==2){ //token = numero
+                        //si todo va bien aca guardaria el tamano size en una global
+                        strcpy(unidad,tokActual->lexema); // global
+                        avanzarToken();
+                    }else{
+                        error=1;
+                        avanzarToken();
+                        printf("se esperaba un numero positivo despues de ::.\n");
+                        break;
+                    }
+                    PAR();
+                }else{
+                   printf("El parametro unit no se puede repetir.\n");
+                   error=1;
+                }
+            break;
+            }
+            default:{
+                printf("El paramentro despues de \"+\" no es correcto.\n");
+                error=1;
+            break;
+            }
 
+        }
+    }
 
 }
+
+void realizarAcciones(){
+    origen();
+    //metodo que borra la tabla de tokens para analizar la siguiente linea.
+}
+
 
